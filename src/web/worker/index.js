fc2059697af387f2b36af85f5baf78de8c6d3cb7
lorypelli <lorypelli@proton.me>
2026-06-07 11:54:11 +0200

    simplified worker index file

--- src/web/worker/index.js
+++ src/web/worker/index.js
@@ -15,54 +15,40 @@ export default {
                     status: 200,
                     headers: {
                         'Content-Type': 'text/markdown; charset=utf-8',
-                        'x-markdown-tokens': tokenCount.toString(),
+                        'X-Markdown-Tokens': tokenCount.toString(),
                         'Vary': 'Accept',
                     },
                 });
             }
         }
         const response = await env.ASSETS.fetch(request);
-        if (!response.ok) {
-            return Response.redirect(new URL('/', request.url).toString(), 302);
-        }
-        if (url.pathname == '/guida.pdf') {
-            const newResponse = new Response(response.body, response);
-            newResponse.headers.set('Content-Disposition', 'inline; filename="guida.pdf"');
-            return newResponse;
-        }
-        if (url.pathname == '/class.png') {
-            const newResponse = new Response(response.body, response);
-            newResponse.headers.set('Content-Disposition', 'inline; filename="class.png"');
-            return newResponse;
-        }
-        if (url.pathname == '/er.png') {
-            const newResponse = new Response(response.body, response);
-            newResponse.headers.set('Content-Disposition', 'inline; filename="er.png"');
-            return newResponse;
-        }
-        if (url.pathname == '/usecase.png') {
-            const newResponse = new Response(response.body, response);
-            newResponse.headers.set('Content-Disposition', 'inline; filename="usecase.png"');
-            return newResponse;
-        }
         if (url.pathname == '/download') {
-            const url = 'https://github.com/LoryPelli/JDER/releases/download/JDER/JDER.jar';
-            const res = await fetch(url);
-            if (!res.ok) {
+            const ghUrl = 'https://github.com/LoryPelli/JDER/releases/download/JDER/JDER.jar';
+            const ghResponse = await fetch(ghUrl);
+            if (!ghResponse.ok) {
                 return Response.redirect('https://github.com/LoryPelli/JDER/releases/latest', 302);
             }
-            const newResponse = new Response(res.body, res);
-            newResponse.headers.set('Content-Disposition', 'attachment; filename="JDER.jar"');
-            return newResponse;
+            return new Response(ghResponse.body, {
+                status: ghResponse.status,
+                statusText: ghResponse.statusText,
+                headers: ghResponse.headers,
+            });
         }
         if (url.pathname == '/' || url.pathname == '/index.html') {
-            const newResponse = new Response(response.body, response);
-            newResponse.headers.set(
+            const headers = new Headers(response.headers);
+            headers.set(
                 'Link',
                 '<https://github.com/LoryPelli/JDER>; rel="service-doc", </sitemap.xml>; rel="sitemap"',
             );
-            newResponse.headers.set('Vary', 'Accept');
-            return newResponse;
+            headers.set('Vary', 'Accept');
+            return new Response(response.body, {
+                status: response.status,
+                statusText: response.statusText,
+                headers,
+            });
+        }
+        if (!response.ok) {
+            return Response.redirect(new URL('/', request.url).toString(), 302);
         }
         return response;
     },
